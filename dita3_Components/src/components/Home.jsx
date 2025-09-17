fe5f84510377328d5db90f3b0f01ed9afe7d1114
Artii04 <artosmani14@gmail.com>
2025-09-17 11:29:00 +0200

    react

---
+++ dita3_Components/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+function Home() {
+  return (
+    <>
+        <h1>Home Page</h1>
+        <p>Hello World!</p>
+        <Button />
+    </>
+  )
+}
+
+export default Home
