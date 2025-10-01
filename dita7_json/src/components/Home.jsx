2324b8177245dbf31bb36de7de0112d4b4100817
Artii04 <artosmani14@gmail.com>
2025-10-01 11:25:11 +0200

    reactt

---
+++ dita7_json/src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import BlogList from './BlogList'
+import ViewList from './ViewList'
+
+function Home() {
+  const [list, setList] = useState([
+    {id: 1, name:"Amr", age:25, city:"Prizren"},
+    {id: 2, name:"Arianit", age:31, city:"Prizren"},
+    {id: 3, name:"Ardian", age:29, city:"Prizren"},
+    {id: 4, name:"Arbnor", age:28, city:"Prizren"},
+    {id: 5, name:"Arlind", age:27, city:"Prizren"},
+  ])
+
+  useEffect(() => {
+    console.log("Components loaded");
+    console.log(list);
+  }, []);
+
+  const deleteButton = (id) =>{
+    const newList = list.filter(list => list.id !== id);
+    setList(newList);
+  }
+
+  return (
+    <>
+        {/* {list.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>Age: {item.age}</p>
+            <p>City: {item.city}</p>
+          </div>
+        ))} */}
+        <BlogList  list={list} x ="Lista e Nxenesve" deleteButton={deleteButton}/>
+    </>
+  )
+}
+
+export default Home
