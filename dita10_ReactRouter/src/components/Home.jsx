d2b8123671e5c09569857d98f91e74d211f5d7be
Artii04 <artosmani14@gmail.com>
2025-10-15 11:21:05 +0200

    reacts

---
+++ dita10_ReactRouter/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <div className='CenterDiv'>Home</div>
+      <h1>Home</h1>
+      <p className='CenText'>This is the Home page</p>
+      
+    </>
+  )
+}
+
+export default Home
