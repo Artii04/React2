077cd3e83219d9ed95598d0f2a989132403af992
Artii04 <artosmani14@gmail.com>
2025-10-27 11:18:17 +0100

    react

--- dita10_ReactRouter/src/components/Home.jsx
+++ dita10_ReactRouter/src/components/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Home() {
   return (
     <>
-      <div className='CenterDiv'>Home</div>
       <h1>Home</h1>
       <p className='CenText'>This is the Home page</p>
       
